7f9b14c199139a3f958e56048532d8ae1b8e9d72
Rahul Grover <groverrahul47@gmail.com>
2025-03-31 15:23:10 +0530

    added default profile pic

--- client/src/Redux/user_creds_slice.js
+++ client/src/Redux/user_creds_slice.js
@@ -1,31 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import defaultProfilePic from "../images/profile_pic.jpg";
 
 export const user_creds = createSlice({
-    name: 'user_info',
-    initialState: {
-      username:'',
-      tag:'',
-      profile_pic:"",
-      id:0
+  name: "user_info",
+  initialState: {
+    username: "",
+    tag: "",
+    profile_pic: defaultProfilePic,
+    id: 0,
+  },
+  reducers: {
+    change_username: (state, action) => {
+      state.username = action.payload;
     },
-    reducers: {
-      change_username: (state,action) => {
-        state.username = action.payload
-      },
-      change_tag: (state,action) => {
-        state.tag = action.payload
-      },
-      option_profile_pic: (state,action) => {
-        state.profile_pic = action.payload
-      },
-      option_user_id: (state,action) => {
-        state.id = action.payload
-      },
+    change_tag: (state, action) => {
+      state.tag = action.payload;
     },
-  })
+    option_profile_pic: (state, action) => {
+      state.profile_pic = action.payload || defaultProfilePic;
+    },
+    option_user_id: (state, action) => {
+      state.id = action.payload;
+    },
+  },
+});
 
-  // Action creators are generated for each case reducer function
-export const { change_username,change_tag,option_profile_pic,option_user_id} = user_creds.actions
+// Action creators are generated for each case reducer function
+export const {
+  change_username,
+  change_tag,
+  option_profile_pic,
+  option_user_id,
+} = user_creds.actions;
 
-
-export default user_creds.reducer
+export default user_creds.reducer;
