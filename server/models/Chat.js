0774799a296fd1216b0a0eab78bde58c5985814c
Rakesh <rakesh.31.3@mountblue.tech>
2025-04-01 13:06:12 +0530

    fixed model issue for adding images,videos

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -1,22 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
   server_id: {
     type: String,
-    required: true
+    required: true,
   },
-  channels: [{
-    channel_id: String,
-    channel_name: String,
-    chat_details: [{
-      content: String,
-      sender_id: String,
-      sender_name: String,
-      sender_pic: String,
-      sender_tag: String,
-      timestamp: String
-    }]
-  }]
+  channels: [
+    {
+      channel_id: String,
+      channel_name: String,
+      chat_details: [
+        {
+          content: String,
+          sender_id: String,
+          sender_name: String,
+          sender_pic: String,
+          sender_tag: String,
+          timestamp: String,
+          contentType: {
+            type: String,
+            enum: ["text", "image", "video"],
+            default: "text",
+          },
+        },
+      ],
+    },
+  ],
 });
 
-module.exports = mongoose.model('Chat', chatSchema); 
+module.exports = mongoose.model("Chat", chatSchema);
